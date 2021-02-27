7c2c7fc2e608f00c203a8619b9ab82ed0ff222b4
Igor <Igorasek16@gmail.com>
2021-02-27 02:28:01 +0100

    Small fix bcs I can't sleep with it

--- main.py
+++ main.py
@@ -89,4 +89,4 @@ async def delete_notes():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info", debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info", debug=True)
